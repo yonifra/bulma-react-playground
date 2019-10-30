717514a749ef038a45083e548f771368579da5ba
Jonathan Fraimorice <jonathanfr@wix.com>
2019-10-30 11:08:12 +0200

    fixed bulma button not working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
 import './App.css';
+import { Button } from 'react-bulma-components';
 
 function App() {
   return (
@@ -8,14 +10,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to refresh.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <Button>Click me</Button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
           Learn React
         </a>
       </header>
